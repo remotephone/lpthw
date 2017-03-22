e41a970fd71fbd31a6e56dd71d517d7f74552f08
root <remotephone@gmail.com>
2017-03-21 22:27:26 -0500

    up to ex3 pt 1

---
+++ ch1/ex03-01.py
@@ -0,0 +1,35 @@
+#just text
+print " I Will now count my chickens:"
+
+#This will do some math after printing text. Then it does some math
+#Note the order of operations - first the / and then +
+print "Hens", 25 + 30 / 6
+
+#Order of Operations again. the % = the remainder of the math it's doing/
+#So, in order, 3 divided by 4 = remainer 3 which is multiplied by 25 to equal
+#75 which is then subtracted from 100
+print "Roosters", 100 - 25 * 3 % 4
+
+#print print print
+print "Now I will count the eggs."
+
+# modulus (%) then divide (no decimal!) then add and subtract
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+#print print print
+print "Is it true that 3 + 2 < 5 - 7?"
+
+#test which is less than
+print 3 + 2 < 5 - 7
+
+# print then math
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
