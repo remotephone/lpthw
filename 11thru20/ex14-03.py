7f43533fe9aae714207d2b0805c3236649dabdaa
remotephone <remotephone@gmail.com>
2017-04-11 22:23:50 -0500

    did more stuff

---
+++ 11thru20/ex14-03.py
@@ -0,0 +1,27 @@
+# first argument is the name of the person running the script
+# second arg is the weather - sunny, rainy, etc
+
+from sys import argv
+
+script, user_name, weather = argv
+prompt = '> '
+
+print "Hi %s, I'm the %s script." % (user_name, script)
+print "I'd like to ask you a few questions."
+print "Do you like me %s?" %user_name
+likes = raw_input(prompt)
+
+print "Where do you  live %s?" %user_name
+lives = raw_input(prompt)
+
+print "What kind of computer do you have?"
+computer = raw_input(prompt)
+
+print """
+Alright so you said %r about liking me.
+You live in %r. Not sure where that is.
+And you have a %r computer. Nice.
+Did you know the weather is %r today?
+""" % (likes, lives, computer, weather)
+
+
---
+++ 11thru20/ex15-02.py
@@ -0,0 +1,33 @@
+# Probably good to remember its doing two things, first opening the file and
+# keeping it in memory and the printing the file using read()
+# Import the module
+
+
+from sys import argv
+
+# Define the argv's. The first one is the script name, second is the filename
+# you pass the script when you run it.
+
+script, filename = argv
+
+# Now we open the filename that we passed above.
+
+txt = open(filename)
+
+# Prints the contents of the file.
+
+print "Here's your file %r:" % filename
+print txt.read()
+
+txt.close()
+
+# Prompts the user to enter the name of the file.
+
+print "Type the filename again:"
+file_again = raw_input("> ")
+
+# prints the submitted filename from the prompt.
+txt_again = open(file_again)
+
+print txt_again.read()
+txt_again.close()
