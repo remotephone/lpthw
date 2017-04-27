cd04a0d27ed15a5af1ef51e73db9ea0289883338
computer <remotephone@gmail.com>
2017-04-27 00:24:57 -0500

    36 plan

---
+++ 36thru40/ex36-01.py
@@ -0,0 +1,21 @@
+# Let's make a game.
+
+# You are a dog. You wake up in the morning
+# You have choices. You can pee on the floor, bother your hooman to let you
+# out, or go eat breakfast first.
+
+# If you eat breakfast you can decide next if you go to sleep or go to the
+# bathroom
+
+# If you go to sleep you wake up just in time for dinner.
+
+# If you go to the bathroom you find yourself outside. You potty. There's a
+# piece of old chicken from yesterday's barbecue on the floor. You can choose
+# to eat it or ignore it.
+
+# IF you eat the chicken you get an ashy beard and your owner wags their finger
+# at you. IF you get a finger wagged at you you can either fall into a deep
+# shame or learn from your mistake. Become wiser. You attain enlightenment.
+
+# IF you pee on the floor fingers are wagged. You can either fall into a deep
+# shame or learn from your mistake.
