a955b2f3c3af0d82c860c5c637019f0ee736723f
computer <remotephone@gmail.com>
2017-05-08 23:30:21 -0500

    working in 40

---
+++ 40thru45/ex40a.py
@@ -0,0 +1,13 @@
+#import module mysuff. This is the mystuff.py script in this folder.
+
+import mystuff
+
+
+# Get apple from dinctionary. 
+mystuff['apple']
+
+# From mystuff module, call apple function.
+mystuff.apple()
+
+# from mystuff module, call tangerine. It's a variable, so we don't need ()
+print mystuff.tangerine
---
+++ 40thru45/mystuff.py
@@ -0,0 +1,6 @@
+
+# This is the apple function, used by calling something.apple()
+def apple():
+    print "I AM APPLES!"
+
+# Defining tangerine variable for use later
