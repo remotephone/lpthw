021bd843fce4296b7468aa16287ef0f2ab42634b
computer <remotephone@gmail.com>
2017-06-25 22:54:35 -0500

    some work...

--- 40thru45/ex43.py
+++ 40thru45/ex43.py
@@ -1,25 +1,49 @@
+from sys import exit
+from random import randint
+
 class Scene(object):
 
     def enter(self):
-        pass
+        print "This scene isn't ready yet"
+        exit(1)
 
 class Engine(object):
 
     def __init__(self, scene_map):
-        pass
+       self.scene_map = scene_map 
 
     def play(self):
-        pass
+       current_scene = self.scene_map.opening_scene()
+       last_scene = self.scene_map.next_scene('finished')
+
+    while current_scene != last_scene:
+        next_scene_name = current_scene.enter()
+        current_scene = self.scene_map.next_scene(next_scene_name)
+
+    current_scene.enter()
 
 class Death(Scene):
 
+
+    quips = [
+            "You died. lame",
+            "You dead son",
+            "You won! jk you died",
+            "good job. losing"
+    ]
+
+
     def enter(self):
-        pass
+       print Death.quips[randint(0, len(self.quips)-1)]
+       exit(1)
 
 class CentralCorrdior(Scene):
 
     def enter(self):
-        pass
+       print "You enter the corridor. Everythings dead and lame but here you
+       are all dumb. You need to get to the armory, get a bomb, put in the
+       brudge and blow the ship up. If you want to survive you need to get into
+       an escpate pod too."
 
 class LaserWeaponArmory(Scene):
 
@@ -50,4 +74,4 @@ class Map(object):
 
 a_map = Map('central_corridor')
 a_game  = Engine(a_map)
-a_game. play()
+a_game.play()
